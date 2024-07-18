cbaacf791d0aa00842a4c2738f83f5b0385bff59
nikos <nikgiannsm@gmail.com>
2024-07-18 14:18:09 +0300

    Added ejs connection

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs");
+const path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
 app.get("/", (req, res) => {
-  res.send("Hello finally!");
+  res.render("index");
 });
 
 module.exports = app;
